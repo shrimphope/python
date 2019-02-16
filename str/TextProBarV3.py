ed8068455acd742f5a8f2c518bb7e44775012dbf
cimex7 <541904881@qq.com>
2019-02-16 20:10:11 +0800

    Update TextProBarV3.py

--- str/TextProBarV3.py
+++ str/TextProBarV3.py
@@ -1,3 +1,24 @@
+文本进度条 不刷新
+#TextProBarV1.py
+import time
+scale = 10
+print("------执行开始------")
+for i in range(scale+1):
+    a = '*' * i
+    b = '.' * (scale - i)
+    c = (i/scale)*100
+    print("{:^3.0f}%[{}->{}]".format(c,a,b))
+    time.sleep(0.1)
+print("------执行结束------")
+
+文本进度条 单行数字刷新
+#TextProBarV2.py
+import time
+for i in range(101):
+    print("\r{:3}%".format(i), end="")
+    time.sleep(0.1)
+
+文本进度条 完整效果
 #TextPorBarV3.py
 import time
 scale = 50
